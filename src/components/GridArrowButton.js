20ec59d7ef5efcdcdbf3dacf311ea4c228ee1c14
JordanSteeleGIT <39620858+JordanSteele96@users.noreply.github.com>
2021-09-28 18:41:16 +0100

    added button icons

--- src/components/GridArrowButton.js
+++ src/components/GridArrowButton.js
@@ -1,10 +1,35 @@
 import React from "react";
+import { FiArrowUpCircle } from "react-icons/fi";
 
 const GridArrowButtons = ({ ClassText }) => {
+  function rotateAmount(value) {
+    switch (value) {
+      case 0:
+        return 135;
+      case 1:
+      case 2:
+      case 3:
+        return 180;
+      case 4:
+        return 225;
+      case 5:
+      case 6:
+      case 7:
+        return 90;
+      default:
+        return 0;
+    }
+  }
   return (
     <>
       <div className={`button button${ClassText}`}>
-        <button>click me</button>
+        <button
+          className="player-choice-button"
+          style={{ transform: `rotate(${rotateAmount(ClassText)}deg)` }}
+          onClick={() => console.log(ClassText)}
+        >
+          <FiArrowUpCircle size="45px" />
+        </button>
       </div>
     </>
   );
